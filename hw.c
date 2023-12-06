6d171a61cc86cae5001d719eaedaf4f81ce6c2f8
andy7601 <andyphu7601@gmail.com>
2023-12-06 00:02:02 -0800

    delays added to main

--- hw.c
+++ hw.c
@@ -16,35 +16,48 @@ int itemCount = 0;
 struct ProducerAttributes {
     int id;
     int count; //keeps track of how many items each producer produces 
-    int delay;
     int* buffer;
+    int bufferSize;
 };
 
 void *producer(void *arg) {
     struct ProducerAttributes *producerArray = (struct ProducerAttributes *)arg;
     int producerID = producerArray->id;
-    int delay = producerArray->delay;
-    
+    int b = producerArray->bufferSize;
     //while the buffer is full continue waiting til the notFull signal is given from the consumer 
     while (1) {
         pthread_mutex_lock(&mutex);
         while (itemCount == MAX_BUFFER_SIZE) {
             pthread_cond_wait(&notfull, &mutex);
         }
-        //Produces an item and adds it to the buffer
-        (producerArray->count)++;
-        //Signal notempty
-        itemCount++;
-        pthread_cond_signal(&notempty);
+
+        //iterate throughout to find an empty spot
+        for(int i = 0; i < b;i++){
+            if (producerArray->buffer[i] == -1){
+                producerArray->buffer[i] = producerArray->count;
+                //Produces an item and adds it to the buffer
+                itemCount++;
+                //Signal notempty
+                (producerArray->count)++;
+                pthread_cond_signal(&notempty);
+                // Break the loop after placing the item in the buffer
+                break; 
+            }
+        }
+
         pthread_mutex_unlock(&mutex);
 
-        usleep(delay);
+        if (delay == 1){
+            usleep(500000);
+        }
     }
     return NULL;
 }
 
 void *consumer(void *arg) {
-    int *delay = (int *)arg;
+    struct ProducerAttributes *producerArray = (struct ProducerAttributes *)arg;
+    int producerID = producerArray->id;
+    int b = producerArray->bufferSize;
 
     while (1) {
         pthread_mutex_lock(&mutex);
@@ -52,13 +65,22 @@ void *consumer(void *arg) {
         while (itemCount == 0) {
             pthread_cond_wait(&notempty, &mutex);
         }
+
+        //iterate throughout to find a filled spot
+        for(int i = 0; i < b;i++){
+            if (producerArray->buffer[i] != -1){
+                producerArray->buffer[i] = -1; //initialize the consumed spot back to -1
+                //Produces an item and adds it to the buffer
+                itemCount--;
+                //Signal notFull after a consumption
+                pthread_cond_signal(&notfull);
+            }
+        }
+
         
-        itemCount--;
-        //Signal notFull after a consumption
-        pthread_cond_signal(&notfull);
         pthread_mutex_unlock(&mutex);
 
-        usleep(*delay);
+
     }
     return NULL;
 }
@@ -80,8 +102,11 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-
+    //initialize buffer if the element is -1 then it's not filled 
     int *buffer = malloc(b * sizeof(int));
+    for(int i = 0; i < sizeof(buffer);i++){
+        buffer[i] = -1; 
+    }
     pthread_t producer_threads[p];
     pthread_t consumer_threads[c];
     struct ProducerAttributes *producerArray = malloc(p * sizeof(struct ProducerAttributes));
@@ -90,22 +115,27 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < p; ++i) {
         producerArray[i].id = i;
         producerArray[i].count = 0;
-        producerArray[i].delay = d;
         producerArray[i].buffer = buffer; // Assign the buffer to each producer so that both functions can use it 
-
+        producerArray[i].bufferSize = b;
     }
 
-    int producer_delay = d;
-    int consumer_delay = d;
+
+    int delay = 500000; 
 
     for (int i = 0; i < p; i++) {
         // Create producer threads
         pthread_create(&producer_threads[i], NULL, producer, (void *)&producerArray[i]);
+        if (d == 1){
+            usleep(delay);
+        }
     }
 
     for (int i = 0; i < c; i++) {
         // Create consumer threads
         pthread_create(&consumer_threads[i], NULL, consumer, (void *)&producerArray[i]);
+        if (d == 0){
+            usleep(delay);
+        }
     }
 
     for (int i = 0; i < p; i++) {
